40b8d132556fea75422c99f8634967b2c996b896
linyongji <lyj18391569527@163.com>
2021-01-12 08:25:21 +0800

    忽略了dev目录下不重要的文件

--- dev/dev/main.cpp
+++ dev/dev/main.cpp
@@ -3,7 +3,7 @@
 #include "dev.h"
 int main()
 {
-	int a = 2147483647, b = 2;
+	int a = 2147483647, b = 3;
 	add_api(a, b);//调用 加法接口add_api
 	sub_api(a, b);//调用 减法接口sub_api
 	mul_api(a, b);	//调用 乘法接口mul_api
